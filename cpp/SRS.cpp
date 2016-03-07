6a60c9931bfedb9a0264b2d84b6116664bd3c384
Kyle <ckgalloway@crimson.ua.edu>
2016-03-07 06:40:41 -0600

    commit

--- cpp/SRS.cpp
+++ cpp/SRS.cpp
@@ -1,25 +1,42 @@
 #include <iostream>
 #include <string>
 #include <time>
-#include "includes/graph"
+#include "StanfordCPPLib/basicgraph.h"
 #include "includes/file_io"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+    /* Grab Filename */
     if(argc != 2) {
         printf("Usage: %s <input filename>\n", argv[0]);
         exit(1);
     }
 
-    Graph G = new Graph();
+    /* Make a new graph */
+    Graph G = new BasicGraph();
+
+    /* Read in a file */
     FileIO file = new FileIO(argv[1]);
+
+    /* Add the contents of the file to the graph. (Change to local) */
     G.buildGraph(file);
     cout << "\n";
+
+    /* Rate the teams in the graph. (Change to local) */
     G.rateTeams();
     cout << "\n";
+
+    /*
+        Get the list of vertexes
+        Can be done much more easily using the new basicgraph.h
+    */
     Dict teams = G.getTeams();
+
+    /* Get the team names (Possibly unneeded due to better Dict in C++ */
     string[] teamNames = G.getTeamNames();
+
+    /* print the ratings to the output */
     file.printRatings(teamNames,teams);
     cout << "\n";
 
